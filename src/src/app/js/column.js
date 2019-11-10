5a20aed851da4956463623f2ecd9eb8784606ac1
Liza Andreevna <karlsonzp2000@gmail.com>
2019-11-10 21:37:05 +0200

    complete create column

--- src/src/app/js/column.js
+++ src/src/app/js/column.js
@@ -1,26 +1,34 @@
-import {clickBut, hideForm} from './card'
-
 class Column {
     constructor(col_name) {
         this.col_name = col_name;
     }
     static newColumn(column) {
+        this.createTask()
         this.createForm()
         this.createButton()
         const main = document.querySelector('.kanban_board');
         const col = document.createElement('div')
-        const task_list = document.createElement ('ul')
         const form = document.querySelector('#new_task')
         const create_task = document.querySelector('#add-task')
+        const taskList = document.querySelector('.task-list')
         col.className = 'card'
         col.id = 'new_card'
         const title = document.createElement('h3')
         title.innerHTML =`<h3>${column.col_name}</h3> `
         main.appendChild(col)
         col.appendChild(title)
-        col.appendChild(task_list)
         col.appendChild(form)
         col.appendChild(create_task)
+        col.appendChild(taskList)
+    }
+    static createTask() {
+        const col = document.querySelector('.card');
+        const task_list = document.createElement ('div');
+        task_list.className = 'task-list'
+        const taskCont = document.createElement ('div');
+        taskCont.className = 'task-container'
+        col.appendChild(task_list)
+        task_list.appendChild(taskCont)
     }
     static createForm() {
         const col = document.querySelector('.card')
@@ -29,7 +37,15 @@ class Column {
         inp_cont.className = 'input_container'
         const input =  document.createElement('input')
         const button = document.createElement('button')
+        const cansel = document.createElement('img')
+        cansel.onclick = () => {
+            form.style.display = 'none'
+            this.showButton()
+        }
+        cansel.className = 'close'
+        cansel.src = 'src/public/img/cross.png'
         button.className = 'task-button'
+        button.textContent = 'Добавить карточку'
         input.placeholder = 'Введите название карточки'
         input.className = 'input'
         form.id = 'new_task'
@@ -38,6 +54,7 @@ class Column {
         form.appendChild(inp_cont)
         form.appendChild(button)
         inp_cont.appendChild(input)
+        form.appendChild(cansel)
     }
     static createButton () {
         const col = document.querySelector('.card')
@@ -47,6 +64,7 @@ class Column {
         create_task.id = 'add-task'
         create_task.onclick = function() {
             form.style.display = 'block'
+            create_task.style.display= 'none';
         }
         const span = document.createElement('span')
         span.innerHTML = 'Добавить ещё одну карточку'
@@ -56,6 +74,11 @@ class Column {
         create_task.appendChild(img)
         create_task.appendChild(span)
     }
+    static showButton() {
+        document.querySelectorAll('.add-task').forEach((el)=>{
+            el.style.display = 'block'
+        })
+    }
 }
 class columnUI {
     static displayColumn() {
@@ -64,7 +87,6 @@ class columnUI {
             Column.newColumn(column)
         });
     }
-    
 }
 class Store {
     static getColumn() {
@@ -84,7 +106,6 @@ class Store {
 }
 
 document.addEventListener('DOMContentLoaded', columnUI.displayColumn);
-
 
 const new_column = document.getElementById('new_column')
 new_column.addEventListener('submit', (e) => {
